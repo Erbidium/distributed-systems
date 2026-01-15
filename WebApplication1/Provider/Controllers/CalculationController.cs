5dc9b704f19454af42c1b33a6a8f104c62100e4b
Erbidium <OlexandrTanasienko@gmail.com>
2026-01-16 00:08:41 +0200

    fix: remove docker compose

--- WebApplication1/Provider/Controllers/CalculationController.cs
+++ WebApplication1/Provider/Controllers/CalculationController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shared.Contracts;
-using Shared.Grpc;
 using System.Diagnostics;
 
 namespace Provider.Controllers;
